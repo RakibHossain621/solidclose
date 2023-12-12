76cc37dcbb996c9d833927bafc1f60d3bd0d0a87
jsdevrazu <islamrajulearn.dev@gmail.com>
2023-12-12 10:09:00 +0600

    fixed sidebar issue

--- js/app.js
+++ js/app.js
@@ -1,20 +1,45 @@
-const gearButton = document.querySelector('.gear-tab');
-const menuButton =  document.querySelector('.hamburger_menu');
+const gearButton = document.querySelector(".gear-tab");
+const menuButton = document.querySelector(".hamburger_menu");
+const sidebar = document.querySelector(".sidebar");
+const overlay = document.querySelector(".overlay");
+const close_button = document.querySelector(".close_button");
 
-if (localStorage.getItem('darkMode') === 'enabled') {
-    document.body.classList.add('dark-mode');
+if (localStorage.getItem("darkMode") === "enabled") {
+  document.body.classList.add("dark-mode");
 }
 
-gearButton.addEventListener('click', myFunction);
+gearButton.addEventListener("click", myFunction);
 
 function myFunction() {
-    var element = document.body;
+  var element = document.body;
 
-    if (element.classList.contains("dark-mode")) {
-        element.classList.remove("dark-mode");
-        localStorage.setItem('darkMode', 'disabled');
-    } else {
-        element.classList.add("dark-mode");
-        localStorage.setItem('darkMode', 'enabled');
-    }
+  if (element.classList.contains("dark-mode")) {
+    element.classList.remove("dark-mode");
+    localStorage.setItem("darkMode", "disabled");
+  } else {
+    element.classList.add("dark-mode");
+    localStorage.setItem("darkMode", "enabled");
+  }
 }
+
+function disableScroll() {
+  document.body.style.overflow = "hidden";
+}
+function enableScroll() {
+  document.body.style.overflow = "auto";
+}
+
+function closeMenu() {
+  sidebar.classList.remove("active");
+  overlay.classList.remove("active");
+  enableScroll();
+}
+
+menuButton.addEventListener("click", () => {
+  sidebar.classList.add("active");
+  overlay.classList.add("active");
+  disableScroll();
+});
+
+close_button.addEventListener("click", closeMenu);
+overlay.addEventListener("click", closeMenu);
